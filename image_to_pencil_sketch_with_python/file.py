85e7b5bb356f96d1da11fb1bb0d15fa979794d9a
Bipul-Harsh <bipulharsh123@gmail.com>
2022-04-02 15:57:14 +0530

    task 3 done

---
+++ image_to_pencil_sketch_with_python/file.py
@@ -0,0 +1,62 @@
+# import cv2
+# import matplotlib.pyplot as plt
+# from sklearn.preprocessing import scale
+
+
+# img = cv2.imread('./image.webp')
+# gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# inverted_img = cv2.bitwise_not(gray_img)
+# smooth_img = cv2.GaussianBlur(inverted_img, (21, 21), sigmaX=0, sigmaY=0)
+# def divide(x,y):
+#     return cv2.divide(x, 255-y, scale=256)
+# img = divide(gray_img, smooth_img)
+
+# while True:
+    
+#     cv2.imshow('image', img)
+#     if cv2.waitKey(1) == ord('q'):
+#         break
+# cv2.destroyAllWindows()
+
+import cv2
+import numpy as np
+import argparse
+
+parser = argparse.ArgumentParser(description="Can check pixel value of any point in given photo.")
+parser.add_argument('--input_file', type=str, default="./image.webp", help="(Required) the image file you want to read")
+parser.add_argument('--wt_ratio', type=float, default=1, help="width ratio to resize frame horizontally")
+parser.add_argument("--ht_ratio", type=float, default=1, help="height ratio to resize frame veritcally")
+parser.add_argument("--wt_size", type=int, default=0, help="to provide the frame width size in pixels")
+parser.add_argument("--ht_size", type=int, default=0, help="to provide the frame height size in pixels")
+
+args = parser.parse_args()
+
+INPUT_FILE = args.input_file
+assert INPUT_FILE != None, "Please provide a image file"
+WT_RATIO = args.wt_ratio
+HT_RATIO = args.ht_ratio
+HT_SIZE = args.ht_size
+WT_SIZE = args.wt_size
+WINDOW_NAME = "Color Picker"
+
+image = cv2.imread(INPUT_FILE)
+image = cv2.resize(image, (WT_SIZE,HT_SIZE), fx=WT_RATIO, fy=HT_RATIO)
+assert type(image) != type(None), "Please provide valid image location"
+
+img = cv2.imread('./image.webp')
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+inverted_img = cv2.bitwise_not(gray_img)
+smooth_img = cv2.GaussianBlur(inverted_img, (21, 21), sigmaX=0, sigmaY=0)
+def divide(x,y):
+    return cv2.divide(x, 255-y, scale=256)
+sketch_image = divide(gray_img, smooth_img)
+
+cv2.namedWindow(WINDOW_NAME)
+
+while(True):
+    cv2.imshow(WINDOW_NAME, sketch_image)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cv2.destroyAllWindows()
