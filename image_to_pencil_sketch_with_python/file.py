b4c65956350044d2d878cc8d91d44a0618486db2
Bipul-Harsh <bipulharsh123@gmail.com>
2022-04-02 16:00:49 +0530

    few updates on code

--- image_to_pencil_sketch_with_python/file.py
+++ image_to_pencil_sketch_with_python/file.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import argparse
 
-parser = argparse.ArgumentParser(description="Can check pixel value of any point in given photo.")
+parser = argparse.ArgumentParser(description="Create sketch image of any given photo.")
 parser.add_argument('--input_file', type=str, default="./image.webp", help="(Required) the image file you want to read")
 parser.add_argument('--wt_ratio', type=float, default=1, help="width ratio to resize frame horizontally")
 parser.add_argument("--ht_ratio", type=float, default=1, help="height ratio to resize frame veritcally")
@@ -17,13 +17,12 @@ WT_RATIO = args.wt_ratio
 HT_RATIO = args.ht_ratio
 HT_SIZE = args.ht_size
 WT_SIZE = args.wt_size
-WINDOW_NAME = "Color Picker"
+WINDOW_NAME = "Sketch Maker"
 
-image = cv2.imread(INPUT_FILE)
-image = cv2.resize(image, (WT_SIZE,HT_SIZE), fx=WT_RATIO, fy=HT_RATIO)
-assert type(image) != type(None), "Please provide valid image location"
+img = cv2.imread(INPUT_FILE)
+img = cv2.resize(img, (WT_SIZE,HT_SIZE), fx=WT_RATIO, fy=HT_RATIO)
+assert type(img) != type(None), "Please provide valid image location"
 
-img = cv2.imread('./image.webp')
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 inverted_img = cv2.bitwise_not(gray_img)
 smooth_img = cv2.GaussianBlur(inverted_img, (21, 21), sigmaX=0, sigmaY=0)
